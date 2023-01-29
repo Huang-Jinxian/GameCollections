72503e2dfa30eebbf7c5ecad25c48e764156e509
Huang Jinxian <huangjinxian1996@outlook.com>
2023-01-29 15:35:35 +0800

    Initial Commit

---
+++ Game Collections/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Game Collections
+//
+//  Created by 黄金先 on 2023/1/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Game Collections/Game_CollectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_CollectionsApp.swift
+//  Game Collections
+//
+//  Created by 黄金先 on 2023/1/29.
+//
+
+import SwiftUI
+
+@main
+struct Game_CollectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
